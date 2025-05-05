07e7b150d65a6812e88368bd03854b56adb63723
Z0mbieNick <nicolas.evdokimov@icloud.com>
2025-05-05 20:48:15 +0200

    fixed __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory, current_app
 import os
 
 app = Flask(__name__)
@@ -7,4 +7,6 @@ UPLOAD_FOLDER = os.path.join(os.getcwd(), "instance/uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-from app import views
+
+
+from app import views    # keep this at the end so routes in views are registered
